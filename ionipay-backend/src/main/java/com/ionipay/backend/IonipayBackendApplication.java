5de91c079484ac671424ff2495124a277a6e171c
Ioannis Nikolaou <ioxnikolaou@gmail.com>
2026-02-13 14:09:30 +0100

    project init with curl

---
+++ ionipay-backend/src/main/java/com/ionipay/backend/IonipayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ionipay.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonipayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonipayBackendApplication.class, args);
+	}
+
+}
---
+++ ionipay-backend/src/test/java/com/ionipay/backend/IonipayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionipay.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IonipayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
